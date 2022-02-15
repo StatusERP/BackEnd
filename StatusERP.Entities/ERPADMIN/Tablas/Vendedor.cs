fac8effaafee10042685b39865a0483dd44cff3a
Carlos roberto velasquez <carlosrobertovelasquez@gmail.com>
2022-02-15 16:19:50 -0600

    Primer Coomit de Fork

---
+++ StatusERP.Entities/ERPADMIN/Tablas/Vendedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusERP.Entities.ERPADMIN.Tablas
+{
+    public class Vendedor:EntityBase
+    {
+        public int MyProperty { get; set; }
+    }
+}
