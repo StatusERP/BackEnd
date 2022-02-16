db305cda7db9b8dcc4640d28b80b743c657bed7d
Carlos roberto velasquez <carlosrobertovelasquez@gmail.com>
2022-02-16 09:11:20 -0600

    Se corrige Clase de vendedor con el Conflito

--- StatusERP.Entities/AS/Tablas/Vendedor.cs
+++ StatusERP.Entities/AS/Tablas/Vendedor.cs
@@ -1,29 +1,21 @@
-<<<<<<< HEAD
-﻿using StatusERP.Entities.ERPADMIN.Tablas;
-using System;
-using System.Collections.Generic;
+
+using StatusERP.Entities.ERPADMIN.Tablas;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-=======
-﻿using System;
-using System.Collections.Generic;
->>>>>>> 13e86e0ade518dc5968e8ed86af6e6f6721a1ed0
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-<<<<<<< HEAD
+
+
 namespace StatusERP.Entities.AS.Tablas
 {
     [Table("Vendedores")]
-    public class Vendedor:EntityBase
+    public class Vendedor : EntityBase
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Column("Vendedor")]
-        [Required]  
-       [StringLength(4)]
+        [Required]
+        [StringLength(4)]
         public string CodVendedor { get; set; }
         [Required]
         [StringLength(40)]
@@ -33,13 +25,7 @@ namespace StatusERP.Entities.AS.Tablas
         public string Email { get; set; }
         public bool Activo { get; set; }
         public int ConjuntoId { get; set; }
-        public Conjunto Conjunto { get; set; }  
-=======
-namespace StatusERP.Entities.ERPADMIN.Tablas
-{
-    public class Vendedor:EntityBase
-    {
-        public int MyProperty { get; set; }
->>>>>>> 13e86e0ade518dc5968e8ed86af6e6f6721a1ed0
+        public Conjunto Conjunto { get; set; }
+
     }
 }
