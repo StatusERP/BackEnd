b32091b5f7fbe0a8a17aecdfde0bad5a7b35dabe
Olivia Escalante <olivia.escalante@gmail.com>
2022-02-16 10:47:19 -0600

    Base Clases CP

---
+++ StatusERP.Entities/CP/Tablas/AuxiliarCP.cs
@@ -0,0 +1,10 @@
+﻿using StatusERP.Entities.ERPADMIN.Tablas;
+
+namespace StatusERP.Entities.CP.Tablas
+{
+    public class AuxiliarCP:EntityBase
+    {
+        public int ConjuntoId { get; set; }
+        public Conjunto Conjunto { get; set; }
+    }
+}
---
+++ StatusERP.Entities/CP/Tablas/DocumentoCP.cs
@@ -0,0 +1,10 @@
+﻿using StatusERP.Entities.ERPADMIN.Tablas;
+
+namespace StatusERP.Entities.CP.Tablas
+{
+    public class DocumentoCP:EntityBase
+    {
+        public int ConjuntoId { get; set; }
+        public Conjunto Conjunto { get; set; }
+    }
+}
