25854441e2fa1821ca4de51853bfbe2f314a9382
Carlos roberto velasquez <carlosrobertovelasquez@gmail.com>
2022-03-09 16:15:39 -0600

    Se Adiciona Controller ConsecutivoGlobal

---
+++ StatusERP.DataAccess/Repositories/AS/ConsecutivoGlobalRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using StatusERP.DataAccess.Repositories.AS.Interfaces;
+using StatusERP.Entities.AS.Tablas;
+
+namespace StatusERP.DataAccess.Repositories.AS;
+
+public class ConsecutivoGlobalRepository:StatusERPContextBase<ConsecutivoGlobal>,IConsecutivoGlobalRepository
+{
+    public ConsecutivoGlobalRepository(StatusERPDBContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    public async Task<ICollection<ConsecutivoGlobal>> GetCollectionAsync(int page, int rows)
+    {
+        return await _dbContext.SelectAsync<ConsecutivoGlobal>(page, rows);
+    }
+
+    public async Task<ConsecutivoGlobal?> GetByIdAsync(int id)
+    {
+        return await _dbContext.SelectAsync<ConsecutivoGlobal>(id);
+    }
+
+    public  async Task<int> CreateAsync(ConsecutivoGlobal consecutivoGlobal)
+    {
+        return await _dbContext.InsertAsync(consecutivoGlobal);
+    }
+
+    public async Task<int> UpdateAsync(ConsecutivoGlobal consecutivoGlobal)
+    {
+        await _dbContext.UpdateAsync(consecutivoGlobal, Mapper);
+        return consecutivoGlobal.Id;
+    }
+
+    public async Task<int> DeleteAsync(int id, string userId)
+    {
+        await _dbContext.DeleteAsync(new ConsecutivoGlobal
+        {
+            Id = id,
+            Updatedby = userId
+        });
+        return id;
+    }
+}
---
+++ StatusERP.DataAccess/Repositories/AS/Interfaces/IConsecutivoGlobalRepository.cs
@@ -0,0 +1,12 @@
+﻿using StatusERP.Entities.AS.Tablas;
+
+namespace StatusERP.DataAccess.Repositories.AS.Interfaces;
+
+public interface IConsecutivoGlobalRepository
+{
+    Task<ICollection<ConsecutivoGlobal>> GetCollectionAsync(int page, int rows); 
+    Task<ConsecutivoGlobal?> GetByIdAsync(int id);
+    Task<int> CreateAsync(ConsecutivoGlobal consecutivoGlobal);
+    Task<int> UpdateAsync(ConsecutivoGlobal consecutivoGlobal);
+    Task<int> DeleteAsync(int id,string userId);
+}
