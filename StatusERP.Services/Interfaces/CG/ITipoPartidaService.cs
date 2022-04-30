df8918ce01eca2e06e4f86d3f0d9bc48d52d9ee3
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2022-04-29 18:00:32 -0600

    log

--- StatusERP.Services/Interfaces/CG/ITipoPartidaService.cs
+++ StatusERP.Services/Interfaces/CG/ITipoPartidaService.cs
@@ -7,12 +7,11 @@ namespace StatusERP.Services.Interfaces.CG
     public interface ITipoPartidaService
     {
 
-<<<<<<< HEAD
-=======
+
         Task<BaseResponseGeneric<ICollection<TipoPartida>>> GetAsync(int page, int rows);
         Task<BaseResponseGeneric<TipoPartida>> GetByIdAsync(int id);
        
->>>>>>> 343e921f8bd3599af8b7c54480f909b21c51bdf7
+
         Task<BaseResponseGeneric<int>> CreateAsync(DtoTipoPartida request, string userId, string codTipoPartida);
 
         Task<BaseResponseGeneric<int>> UpdateAsync(int id, DtoTipoPartida request, string userId);
