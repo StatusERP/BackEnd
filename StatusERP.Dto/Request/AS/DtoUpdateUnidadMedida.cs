e96dfc9b913051111a418fc2f099a8198f7812ea
Carlos roberto velasquez <carlosrobertovelasquez@gmail.com>
2022-06-14 16:55:32 -0600

    se adiciona DtoUpdateUnidadMedida

---
+++ StatusERP.Dto/Request/AS/DtoUpdateUnidadMedida.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusERP.Dto.Request.AS
+{
+    public record DtoUpdateUnidadMedida
+    {
+        public string descripcion { get; set; }
+        public string updatedby { get; set; }
+      //  public DateTime updateDate { get; set; }
+    }
+}
