178d758c959fba4c3caf6d663031d29b2885655a
Olivia Escalante <olivia.escalante@gmail.com>
2022-08-18 17:59:54 -0600

    Pendientes4

---
+++ StatusERP.DataAccess/Repositories/AS/Interfaces/IDetalleDireccionRepository.cs
@@ -0,0 +1,14 @@
+﻿using StatusERP.Entities.AS.Tablas;
+
+namespace StatusERP.DataAccess.Repositories.AS
+{
+    public interface IDetalleDireccionRepository
+    {
+        Task<ICollection<DetalleDireccion>> GetCollectionAsync();
+        Task<DetalleDireccion?> GetByIdAsync(int id);
+        Task<int> CreateAsync(DetalleDireccion detalleDireccion);
+        Task<int> UpdateAsync(DetalleDireccion detalleDireccion);
+        Task<int> DeleteAsync(int id, string userId);
+        Task<DetalleDireccion?> BuscarCodDetalleDireccionAsync(int codDetalleDireccion);
+    }
+}
---
+++ StatusERP.DataAccess/Repositories/AS/Interfaces/IDireccionRepository.cs
@@ -0,0 +1,15 @@
+﻿using StatusERP.Entities.AS.Tablas;
+
+namespace StatusERP.DataAccess.Repositories.AS
+
+{
+    public interface IDireccionRepository
+    {
+        Task<ICollection<Direccion>> GetCollectionAsync(int page, int rows);
+        Task<Direccion?> GetByIdAsync(int id);
+        Task<Direccion?> BuscarDireccionAsync(string CodDireccion);
+        Task<int> CreateAsync(Direccion direccion);
+        Task<int> UpdateAsync(Direccion direccion);
+        Task<int> DeleteAsync(int id, string userId);
+    }
+}
