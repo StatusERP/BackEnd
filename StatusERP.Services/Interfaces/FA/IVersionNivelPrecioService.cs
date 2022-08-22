907bf2424de14aa2d74d589642850c9059048647
Olivia Escalante <olivia.escalante@gmail.com>
2022-08-22 17:57:15 -0600

    FaltanteVersionNivelPrecio

---
+++ StatusERP.Services/Interfaces/FA/IVersionNivelPrecioService.cs
@@ -0,0 +1,16 @@
+﻿using StatusERP.Dto.Request.FA;
+using StatusERP.Dto.Response;
+using StatusERP.Entities.FA.Tablas;
+
+namespace StatusERP.Services.Interfaces.FA;
+
+public interface IVersionNivelPrecioService
+{
+    Task<BaseResponseGeneric<ICollection<VersionNivelPrecio>>> GetAsync(string userId);
+    Task<BaseResponseGeneric<VersionNivelPrecio>> GetByIdAsync(int id);
+
+    Task<BaseResponseGeneric<int>> CreateAsync(DtoVersionNivelPrecio request, string userId, int nivelPrecioId, int version);
+    Task<BaseResponseGeneric<int>> UpdateAsync(int id, DtoVersionNivelPrecio request, string userId);
+
+    Task<BaseResponseGeneric<int>> DeleteAsync(int id, string userId);
+}
